c11e683213fd0b972eb299598645f779d948e835
Robert Harris <rharris@twitter.com>
2013-11-09 14:27:41 -0800

    adjusted scales

--- static/js/asa.js
+++ static/js/asa.js
@@ -5,6 +5,7 @@ function Diagram() {
   var MSECS_INA_MIN = 60 * 1000;
   var MSECS_INA_HOUR = 60 * MSECS_INA_MIN;
   var MSECS_INA_DAY = 24 * MSECS_INA_HOUR;
+  var MAX_RADIUS = 20;
 
   // padding and margin vaues
 
@@ -26,7 +27,7 @@ function Diagram() {
   var width;
   var height;
 
-  // standard elements
+  // standard diagram elements
 
   var x;
   var y;
@@ -36,6 +37,10 @@ function Diagram() {
   var marginArea;
   var dataArea;
   var defs;
+
+  // the article index
+
+  var articleIndex = [];
 
   // when document ready, initialize the chart
 
@@ -54,14 +59,9 @@ function Diagram() {
     marginArea = svg.append("g").classed("marginArea", true);
     dataArea = svg.append("g").classed("dataArea", true);
 
-    // append place for svg definitions
-
-
     // create scales and axes
-    var now = new Date();
-    var yesterday = new Date(now.getTime() - MSECS_INA_DAY);
 
-    x = d3.time.scale().domain([yesterday, now]);
+    x = d3.time.scale()
     y = d3.scale.linear();
     xAxis = d3.svg.axis();
     yAxis = d3.svg.axis().orient("right");
@@ -70,13 +70,27 @@ function Diagram() {
 
     size();
 
+    // load the data
+
+    load();
+  }
+
+  function load() {
+
+    // generate random data
+
+    var end = new Date();
+    var start = new Date(end.getTime() - 365 * MSECS_INA_DAY);
+    var dateGen = d3.time.scale().domain([start, end]);
+
+    for (var i = 0; i < 5; ++i) {
+      articleIndex.push({date: dateGen.invert(Math.random()), y: Math.random()});
+    }
+
     // construct static portions of the chart
 
     construct();
-
-    // update the dynamic, data driven, portions of the chart
-
-    // update();
+    update();
   }
 
   // establish all chart size related elements
@@ -96,8 +110,8 @@ function Diagram() {
 
     // update elements
 
-    x.range([0, width]);
-    y.range([0, height]);
+    x.range([MAX_RADIUS, width - MAX_RADIUS]);
+    y.range([MAX_RADIUS, height - MAX_RADIUS]);
     svg.attr("width", svgWidth).attr("height", svgHeight);
     marginArea.attr("transform", "translate(" + padding.left + "," + padding.top + ")");
     dataArea.attr("transform", "translate(" + (padding.left + margin.left) + "," + (padding.top + margin.top) + ")");
@@ -109,17 +123,33 @@ function Diagram() {
   // construt ontime chart specific elements
 
   function construct() {
+    x.domain(d3.extent(articleIndex, function(d) {return d.date}));
 
     dataArea.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-    
+
     dataArea.append("g")
       .attr("class", "y axis")
       .attr("transform", "translate(" + width + ",0)")
       .call(yAxis);
   };
+
+  function update() {
+    dataArea.selectAll("g.article")
+      .data(articleIndex)
+      .enter()
+      .append("g")
+      .classed("article", true)
+      .attr("transform", function(d) {
+        var xt = x(d.date);
+        var yt = y(d.y);
+        return "translate(" + xt + ", " + yt + ")";
+      })
+      .append("circle")
+      .attr("r", MAX_RADIUS);
+  }
 }
 
 $(document).ready(function() {

bce08e989d9cbac0da26b60bfd1a33b9349d9ae5
Robert Harris <rharris@twitter.com>
2013-11-09 13:04:48 -0800

    removed extra boilerplate code and created stock axies

--- static/js/asa.js
+++ static/js/asa.js
@@ -1,9 +1,17 @@
 function Diagram() {
 
-  var padding = {top: 50, right: 50, bottom: 50, left: 50};
+  // constants
+
+  var MSECS_INA_MIN = 60 * 1000;
+  var MSECS_INA_HOUR = 60 * MSECS_INA_MIN;
+  var MSECS_INA_DAY = 24 * MSECS_INA_HOUR;
+
+  // padding and margin vaues
+
+  var padding = {top: 10, right: 10, bottom: 10, left: 10};
   var margin = {top: 25, right: 25, bottom: 25, left: 25};
 
-  // padding region
+  // svg region
 
   var svgWidth;
   var svgHeight;
@@ -29,10 +37,11 @@ function Diagram() {
   var dataArea;
   var defs;
 
-  console.log("padding", padding);
-  console.log("margin", margin);
-
   // when document ready, initialize the chart
+
+  $(document).ready(function() {
+    initialize();
+  });
 
   // perform all chart initialization
 
@@ -49,9 +58,11 @@ function Diagram() {
 
 
     // create scales and axes
+    var now = new Date();
+    var yesterday = new Date(now.getTime() - MSECS_INA_DAY);
 
-    x = d3.scale.identity();
-    y = d3.scale.identity();
+    x = d3.time.scale().domain([yesterday, now]);
+    y = d3.scale.linear();
     xAxis = d3.svg.axis();
     yAxis = d3.svg.axis().orient("right");
 
@@ -68,22 +79,6 @@ function Diagram() {
     // update();
   }
 
-  function debug() {
-    var context = this;
-    [
-      "padding",
-      "margin", 
-      "svgWidth",
-      "svgHeight", 
-      "marginWidth", 
-      "marginHeight", 
-      "width", 
-      "height"
-    ].forEach(function(itemName) {
-      console.log(itemName, context[itemName]);
-    });
-  };
-
   // establish all chart size related elements
 
   function size() {
@@ -99,53 +94,22 @@ function Diagram() {
     width = marginWidth  - margin.left - margin.right;
     height = marginHeight - margin.top - margin.bottom;
 
-    debug();
-
     // update elements
 
-    x.domain([0, width]);
-    y.domain([0, height]);
+    x.range([0, width]);
+    y.range([0, height]);
     svg.attr("width", svgWidth).attr("height", svgHeight);
     marginArea.attr("transform", "translate(" + padding.left + "," + padding.top + ")");
     dataArea.attr("transform", "translate(" + (padding.left + margin.left) + "," + (padding.top + margin.top) + ")");
+
+    xAxis.scale(x);
+    yAxis.scale(y);
   }
 
   // construt ontime chart specific elements
 
   function construct() {
 
-    marginArea.append("rect")
-      .attr("width", marginWidth)
-      .attr("height", marginHeight);
-
-    // construct diagram specific elemenents
-
-    defs.append("marker")
-      .attr("id", "triangle-start")
-      .attr("viewBox", "0 0 10 10")
-      .attr("refX", 10)
-      .attr("refY", 5)
-      .attr("markerWidth", -6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-      .append("path")
-      .attr("d", "M 0 0 L 10 5 L 0 10 z");
-    
-    defs.append("marker")
-      .attr("id", "triangle-end")
-      .attr("viewBox", "0 0 10 10")
-      .attr("refX", 10)
-      .attr("refY", 5)
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-      .append("path")
-      .attr("d", "M 0 0 L 10 5 L 0 10 z");
-    
-    dataArea.append("rect")
-      .attr("width", width)
-      .attr("height", height);
-    
     dataArea.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -155,73 +119,10 @@ function Diagram() {
       .attr("class", "y axis")
       .attr("transform", "translate(" + width + ",0)")
       .call(yAxis);
-    
-    marginArea.append("line")
-      .attr("class", "arrow")
-      .attr("x2", margin.left)
-      .attr("y2", margin.top)
-      .attr("marker-end", "url(#triangle-end)");
-    
-    marginArea.append("line")
-      .attr("class", "arrow")
-      .attr("x1", marginWidth / 2)
-      .attr("x2", marginWidth / 2)
-      .attr("y2", margin.top)
-      .attr("marker-end", "url(#triangle-end)");
-    
-    marginArea.append("line")
-      .attr("class", "arrow")
-      .attr("x1", marginWidth / 2)
-      .attr("x2", marginWidth / 2)
-      .attr("y1", marginHeight - margin.bottom)
-      .attr("y2", marginHeight)
-      .attr("marker-start", "url(#triangle-start)");
-    
-    marginArea.append("line")
-      .attr("class", "arrow")
-      .attr("x2", margin.left)
-      .attr("y1", marginHeight / 2)
-      .attr("y2", marginHeight / 2)
-      .attr("marker-end", "url(#triangle-end)");
-    
-    marginArea.append("line")
-      .attr("class", "arrow")
-      .attr("x1", marginWidth)
-      .attr("x2", marginWidth - margin.right)
-      .attr("y1", marginHeight / 2)
-      .attr("y2", marginHeight / 2)
-      .attr("marker-end", "url(#triangle-end)");
-    
-    svg.append("text")
-      .text("padding area")
-      .attr("x", "2em")
-      .attr("y", "2em");
-
-    marginArea.append("text")
-      .text("margin area")
-      .attr("x", "2em")
-      .attr("y", "2em");
-    
-    dataArea.append("text")
-      .text("data area")
-      .attr("x", "2em")
-      .attr("y", "2em");
-    
-    // marginArea.append("circle")
-    //   .attr("class", "origin")
-    //   .attr("r", 4.5);
-    
-    // dataArea.append("text")
-    //   .text("translate(padding.left, padding.top)")
-    //   .attr("y", -8);
   };
-
-  initialize();
 }
 
 $(document).ready(function() {
-  console.log("pre-ready");
   var d = new Diagram();
-  console.log("post-ready");
 });
 
